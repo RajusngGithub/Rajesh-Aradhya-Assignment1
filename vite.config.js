7b8ee685ada16d2b698855379d75d527191102ee
Rajesh <rajaradhya1704@gmail.com>
2025-08-01 14:20:44 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH || "/Rajesh-Aradhya-Assignment1",
   plugins: [
     react(),
     tailwindcss(),
